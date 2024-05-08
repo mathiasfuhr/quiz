1a9be008a71b74ffc97b1f1b7541911fb1676b33
Mathiasfuhr <mathiasgilvanf@gmail.com>
2024-05-08 10:58:47 -0300

    criando o handleRestartButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
 "use client";
 
 import { QuestionItem } from "@/components/QuestionItem";
+import { Results } from "@/components/Results";
 import { questions } from "@/data/questions";
 import { useState } from "react";
 
 const Page = () => {
+  const [answers, setAnswers] = useState<number[]>([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResults, setShowResults] = useState(false);
   const title = "Teste Seus Conhecimentos em React";
 
-  const handleAnswered = (answer:number) => {}
+  const loadNextQuestion = () => {
+    if (questions[currentQuestion + 1]) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  const handleAnswered = (answer: number) => {
+    setAnswers([...answers, answer]);
+    loadNextQuestion();
+  };
+
+  const handleRestartButton = () => {
+    setAnswers([]);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
 
   return (
     <div className="w-full h-screen flex justify-center items-center bg-blue-600">
@@ -17,14 +37,27 @@ const Page = () => {
           {title}
         </div>
         <div className="p-5">
+          {!showResults && (
             <QuestionItem
-                question={questions[currentQuestion]}
-                count={currentQuestion + 1}
-                onAnswer={handleAnswered}
+              question={questions[currentQuestion]}
+              count={currentQuestion + 1}
+              onAnswer={handleAnswered}
             />
+          )}
+          {showResults && <Results questions={questions} answers={answers} />}
         </div>
         <div className="p-5 text-center border-t border-gray-300">
-          {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? "" : "s"}
+          {!showResults &&
+            `${currentQuestion + 1} de ${questions.length} pergunta
+            ${questions.length === 1 ? "" : "s"}`}
+          {showResults && (
+            <button
+              onClick={handleRestartButton}
+              className="px-3 py-2 rounded-md bg-blue-800 text-white"
+            >
+              Reinciar Quiz
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/data/questions.ts
+++ src/data/questions.ts
@@ -58,45 +58,5 @@ export const questions= [
             'Elementos HTML padrão'
         ],
         answer: 2 // Índice da resposta correta é 2 (a terceira opção)
-    },
-    {
-        question: 'Qual hook do React é usado para adicionar estado a um componente de função?',
-        options: [
-            'useState()',
-            'useEffect()',
-            'useContext()',
-            'useReducer()'
-        ],
-        answer: 0 // Índice da resposta correta é 0 (a primeira opção)
-    },
-    {
-        question: 'Qual método é usado para renderizar vários elementos em React sem a necessidade de um container extra?',
-        options: [
-            'ReactDOM.renderMany()',
-            'React.createElement()',
-            'React.render()',
-            'React.Fragment'
-        ],
-        answer: 3 // Índice da resposta correta é 3 (a quarta opção)
-    },
-    {
-        question: 'Qual é a principal diferença entre componentes de classe e componentes de função em React?',
-        options: [
-            'Os componentes de função não podem ter estado (state)',
-            'Os componentes de classe não podem retornar elementos JSX',
-            'Os componentes de função não possuem métodos do ciclo de vida',
-            'Os componentes de classe são mais rápidos para renderizar'
-        ],
-        answer: 2 // Índice da resposta correta é 2 (a terceira opção)
-    },
-    {
-        question: 'O que é React Router?',
-        options: [
-            'Uma biblioteca para criar animações em componentes React',
-            'Um pacote de ferramentas para testar aplicativos React',
-            'Uma solução para roteamento de URLs em aplicativos React',
-            'Um sistema de gerenciamento de estado global'
-        ],
-        answer: 2 // Índice da resposta correta é 2 (a terceira opção)
     }
 ];
